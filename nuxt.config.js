f34185545ec9ce46bc5427a8de40e04155385eaf
申鹏翔 <spx123456@foxmail.com>
2018-04-10 22:27:06 +0800

    dynamic form

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,24 +4,25 @@ module.exports = {
    */
   head: {
     title: 'giantgo-nuxt',
-    meta: [{
-      charset: 'utf-8'
-    },
+    meta: [
       {
+        charset: 'utf-8'
+      }, {
         name: 'viewport',
         content: 'width=device-width, initial-scale=1'
-      },
-      {
+      }, {
         hid: 'description',
         name: 'description',
         content: 'Nuxt.js project'
       }
     ],
-    link: [{
-      rel: 'icon',
-      type: 'image/x-icon',
-      href: '/favicon.ico'
-    }]
+    link: [
+      {
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: '/favicon.ico'
+      }
+    ]
   },
   /*
    ** Customize the progress bar color
@@ -30,37 +31,30 @@ module.exports = {
     color: '#3B8070'
   },
   css: [
-    'element-ui/lib/theme-chalk/reset.css',
-    'element-ui/lib/theme-chalk/index.css'
+    'element-ui/lib/theme-chalk/reset.css', 'element-ui/lib/theme-chalk/index.css'
   ],
   plugins: [
-    '@/plugins/element-ui',
-    '@/plugins/lodash'
+    '@/plugins/element-ui', '@/plugins/lodash'
   ],
   generate: {
-    routes: [
-      '/forms/1385'
-    ]
+    routes: ['/forms/1385']
   },
   /*
    ** Build configuration
    */
   build: {
-    vendor: ['axios', 'lodash'],
+    vendor: [
+      'axios', 'lodash'
+    ],
     /*
      ** Run ESLint on save
      */
-    extend (config, {
-      isDev,
-      isClient
-    }) {
+    extend (config, {isDev, isClient}) {
       if (isDev && isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
+        config
+          .module
+          .rules
+          .push({enforce: 'pre', test: /\.(js|vue)$/, loader: 'eslint-loader', exclude: /(node_modules)/})
       }
     }
   }
--- pages/forms/_id/index.vue
+++ pages/forms/_id/index.vue
@@ -10,7 +10,50 @@
         :key="field.id"
         :rules="field.rules"
         :error="field.error">
-        <el-input v-model="field.value"></el-input>
+        <el-input v-model="field.value" v-if="field.displayType === '1'"/>
+        <el-input
+          type="textarea"
+          :autosize="{ minRows: 2, maxRows: 4}"
+          v-model="field.value"
+          v-if="field.fieldType.toLowerCase() === '2'">
+        </el-input>
+        <el-input v-model="field.value" type="password" v-if="field.displayType === '10'"/>
+        <el-radio-group v-model="field.value" v-if="field.displayType === '3'">
+          <el-radio
+            v-for="item in field.options"
+            :label="item.optionValue"
+            :key="item.optionId">
+            {{item.option}}
+          </el-radio>
+        </el-radio-group>
+        <el-select v-model="field.value" placeholder="请选择" v-if="field.displayType === '4'" value="">
+          <el-option
+            v-for="item in field.options"
+            :key="item.optionId"
+            :label="item.optionValue"
+            :value="item.optionId">
+          </el-option>
+        </el-select>
+        <el-checkbox-group v-model="field.value" v-if="field.displayType === '5'">
+          <el-checkbox
+            v-for="item in field.options"
+            :label="item.optionValue"
+            :key="item.optionId">
+            {{item.optionValue}}
+          </el-checkbox>
+        </el-checkbox-group>
+        <el-upload
+          v-if="field.displayType === '7' || field.displayType === '8'"
+          class="avatar-uploader"
+          action="https://jsonplaceholder.typicode.com/posts/"
+          :show-file-list="false"
+          :on-success="handleAvatarSuccess"
+          :before-upload="beforeAvatarUpload">
+          <img v-if="field.url" :src="field.url" class="avatar">
+          <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+        </el-upload>
+        <el-date-picker
+          type="datetime" v-model="field.value" v-if="field.fieldType.toLowerCase() === 'datetime'"/>
       </el-form-item>
     </el-form>
     <el-button type="primary" @click.native="submit" :loading="isSubmitting">保存</el-button>
@@ -30,25 +73,47 @@
     },
     methods: {
       submit () {
-        this.$refs['form'].validate((valid) => {
+        this.$refs['form'].validate(valid => {
           if (!valid) {
             return
           }
 
           console.log(this.form)
         })
+      },
+      handleAvatarSuccess (res, file) {
+        this.imageUrl = URL.createObjectURL(file.raw)
+      },
+      beforeAvatarUpload (file) {
+        const isJPG = file.type === 'image/jpeg'
+        const isLt2M = file.size / 1024 / 1024 < 2
+
+        if (!isJPG) {
+          this.$message.error('上传头像图片只能是 JPG 格式!')
+        }
+        if (!isLt2M) {
+          this.$message.error('上传头像图片大小不能超过 2MB!')
+        }
+        return isJPG && isLt2M
       }
     },
     async asyncData ({params}) {
       try {
-        let {data} = await axios.post('http://uat-api.smarket.net.cn/member/form/GetForNewForm', {
-          memberFormId: params.id
-        })
+        let {data} = await axios.post(
+          'http://s2-api.smarket.net.cn/member/form/GetForNewForm',
+          {
+            memberFormId: params.id
+          }
+        )
 
         let form = data.body.content
 
         _.forEach(form.items, item => {
-          item.value = ''
+          item.value = ['5', '6'].indexOf(item.displayType) > -1 ? [] : ''
+
+          if (['7', '8'].indexOf(item.displayType) > -1) {
+            item.url = undefined
+          }
         })
 
         return {
@@ -57,8 +122,7 @@
             fields: form.items
           }
         }
-      }
-      catch (e) {
+      } catch (e) {
         console.log(e)
       }
     }
@@ -66,5 +130,30 @@
 </script>
 
 <style>
+  .avatar-uploader .el-upload {
+    border: 1px dashed #d9d9d9;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+  }
 
+  .avatar-uploader .el-upload:hover {
+    border-color: #409eff;
+  }
+
+  .avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 178px;
+    height: 178px;
+    line-height: 178px;
+    text-align: center;
+  }
+
+  .avatar {
+    width: 178px;
+    height: 178px;
+    display: block;
+  }
 </style>
