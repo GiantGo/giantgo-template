84d975d5142dd01793d7021f234dc04865de3a0a
siler_shen <spx123456@foxmail.com>
2019-01-12 22:51:05 +0800

    add ad url

--- pages/channel/_id.vue
+++ pages/channel/_id.vue
@@ -73,15 +73,15 @@
     },
     methods: {},
     head: {
-      script: [
-        {
-          innerHTML: `(adsbygoogle = window.adsbygoogle || []).push({
-  google_ad_client: 'ca-pub-3164628254889272',
-  enable_page_level_ads: true
-})`, type: 'text/javascript'
-        }
-      ],
-      __dangerouslyDisableSanitizers: ['script']
+//       script: [
+//         {
+//           innerHTML: `(adsbygoogle = window.adsbygoogle || []).push({
+//   google_ad_client: 'ca-pub-3164628254889272',
+//   enable_page_level_ads: true
+// })`, type: 'text/javascript'
+//         }
+//       ],
+//       __dangerouslyDisableSanitizers: ['script']
     },
     async asyncData ({params}) {
       try {
